d12ca60625cd2c766ad876db99a91bcefab8ede6
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-03-13 15:27:15 +0300

    refactoring

--- intercepting-page.js
+++ intercepting-page.js
@@ -1,10 +1,43 @@
+// Get intercepted URL
 var interceptedUrl = localStorage.getItem("interceptedUrl") || 'http://google.com';
 localStorage.removeItem("interceptedUrl");
 
-$(document).ready(function () {
-    bindEvents();
-    render();
-});
+// Decide vocabulary
+if (vocabulary.globish.length == 0) {
+    console.log("No vocabulary data! Redirecting directly to intercepted url...");
+    goToInterceptedUrl();
+}
+
+var randomVocabulary = {};
+var retryCount = 0;
+var isProperToShow = false;
+do {
+    if (retryCount > 10) {
+        break;
+    }
+    retryCount++;
+
+    var randomIndex = Math.floor((Math.random() * vocabulary.globish.length));
+    randomVocabulary = vocabulary.globish[randomIndex];
+    var definition = randomVocabulary.definitions && randomVocabulary.definitions['english'];
+    isProperToShow = definition && randomVocabulary.definitions['english'].trim().length > 0;
+    if (!isProperToShow) {
+        console.log("Vocabulary was not proper to show: " + JSON.stringify(randomVocabulary));
+    }
+} while (!isProperToShow);
+
+if (!isProperToShow) {
+    console.log("No proper vocabulary found to show after lots of trying! Redirecting directly to intercepted url...");
+    goToInterceptedUrl();
+}
+
+// Prepare UI if vocabulary proper to display
+if (isProperToShow) {
+    $(document).ready(function () {
+        bindEvents();
+        renderSticky();
+    });
+}
 
 function bindEvents() {
     $(document).on('click', '.blurry-text', function () {
@@ -20,52 +53,24 @@ function bindEvents() {
     });
 }
 
-function render() {
-    var template =
-        '<p class="word">{{word}}</p>' +
-        '<p class="definition">{{definition}}</p>' +
-        '{{translation}}' +
+function renderSticky() {
+    // Render examples part
+    var examplesContent = "";
+    var examplesTemplate =
         '<p class="title">Example usages:</p>' +
         '<ul class="example-usages">' +
         '   {{examples}}' +
         '</ul>';
-
-    if (vocabulary.globish.length == 0) {
-        console.log("No vocabulary data! Redirecting directly to intercepted url...");
-        goToInterceptedUrl();
-        return;
-    }
-
-    var randomVocabulary = {};
-    var retryCount = 0;
-    var isProperToShow = false;
-    do {
-        if (retryCount > 10) {
-            break;
-        }
-        retryCount++;
-
-        var randomIndex = Math.floor((Math.random() * vocabulary.globish.length));
-        randomVocabulary = vocabulary.globish[randomIndex];
-        var definition = randomVocabulary.definitions && randomVocabulary.definitions['english'];
-        isProperToShow = definition && randomVocabulary.definitions['english'].trim().length > 0;
-        if (!isProperToShow) {
-            console.log("Vocabulary was not proper to show: " + JSON.stringify(randomVocabulary));
-        }
-    } while (!isProperToShow);
-
-    if (!isProperToShow) {
-        console.log("No proper vocabulary found to show after lots of trying! Redirecting directly to intercepted url...");
-        goToInterceptedUrl();
-        return;
-    }
-
-    var examplesContent = "";
     for (var i = 0; i < randomVocabulary.examples.length; i++) {
         examplesContent += '<li>' + randomVocabulary.examples[i] + '</li>';
     }
+    if (examplesContent && examplesContent != "") {
+        examplesContent = examplesTemplate.replace("{{examples}}", examplesContent);
+    }
 
+    // Render translation part
     var translationContent = "";
+
     var translation = randomVocabulary.definitions['turkish'];
     if (translation && translation.length > 0) {
         translationContent =
@@ -73,13 +78,20 @@ function render() {
             '<p class="translation blurry-text">' + translation + '</p>';
     }
 
-    var renderResult = template
+    // Render the sticky
+    var stickyTemplate =
+        '<p class="word">{{word}}</p>' +
+        '<p class="definition">{{definition}}</p>' +
+        '{{translation}}' +
+        '{{examples}}';
+
+    var renderingResult = stickyTemplate
         .replace('{{word}}', randomVocabulary.word)
         .replace('{{definition}}', randomVocabulary.definitions['english'])
         .replace('{{translation}}', translationContent)
         .replace('{{examples}}', examplesContent);
 
-    $('div.container').append($(renderResult));
+    $('div.container').append($(renderingResult));
 
     $('.quote-container').show();
 }
