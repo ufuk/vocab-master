b726981191d3d530ba77b60ecd5fe224fdb51bad
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-03-22 21:31:49 +0300

    fix utf-8 encoding problem & style polishing

--- options.js
+++ options.js
@@ -14,7 +14,7 @@ function saveOptions() {
     displayStatus("Options saved.");
 
     chrome.runtime.sendMessage({message: "newOptionsSaved"}, function (response) {
-        console.log(response.message);
+        // DO nothing for now
     });
 }
 
@@ -78,4 +78,4 @@ function displayStatus(statusText) {
 
 // Set event bindings
 document.addEventListener('DOMContentLoaded', restoreOptions);
-$( 'input , select' ).change( saveOptions );
+$('input , select').change(saveOptions);
